07558c941e328c1c6a0d6979dd112ff51121c53d
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-01-20 09:55:05 +0100

    .

--- EsercizioNatale3/MainWindow.xaml.cs
+++ EsercizioNatale3/MainWindow.xaml.cs
@@ -20,9 +20,61 @@ namespace EsercizioNatale3
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        private TextBox[] array;
         public MainWindow()
         {
             InitializeComponent();
+            array = new TextBox[] { txt1, txt2, txt3, txt4, txt5, txt6 };
+        }
+        
+        private void btnONOFF_Click(object sender, RoutedEventArgs e)
+        {
+            if(btnONOFF.Content.ToString() == "ON")
+            {
+                for(int i=0;i<array.Length; i++)
+                {
+                    array[i].IsEnabled = true;
+                }
+                btncalcola.IsEnabled = true;
+                btnONOFF.Content = "OFF";
+            }
+            else 
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    array[i].IsEnabled = false;
+                }
+                btncalcola.IsEnabled = false;
+                btnONOFF.Content = "ON";
+            }
+
+        }
+
+        private void btncalcola_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int c1 = 0;
+                int c2 = 0;
+                for(int i = 0; i < array.Length; i++)
+                {
+                    int num = int.Parse(array[i].Text);
+                    if (num % 2 == 0)
+                    {
+                        c1++;
+                    }
+                    else
+                    {
+                        c2++;
+                    }
+                }
+                lblris.Content = $"I numeri pari sono {c1}, i numeri dispari sono {c2}";
+            }
+            catch
+            {
+                MessageBox.Show("Inserire solo numeri", "Attenzione", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+            }
         }
     }
 }
